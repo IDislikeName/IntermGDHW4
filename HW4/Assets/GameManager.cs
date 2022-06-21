4dfc5b0e148ba95f0842d2592d21c8e4e283605f
IDislikeName <54780075+IDislikeName@users.noreply.github.com>
2022-06-21 00:13:23 -0400

    2

--- HW4/Assets/GameManager.cs
+++ HW4/Assets/GameManager.cs
@@ -30,7 +30,24 @@ public class GameManager : MonoBehaviour
     public float currentCD=0;
     private void Update()
     {
-        
+        currentCD -= Time.deltaTime;
+        if (currentCD <= 0)
+        {
+            currentCD = spawnCD;
+            GameObject boat = Instantiate(enemyBoat);
+            float rand = Random.Range(0, 2);
+            if (rand ==1)
+            {
+                boat.transform.position = new Vector2(-7,Random.Range(-2,-5));
+                boat.GetComponent<Rigidbody2D>().velocity = new Vector2(2.5f,0);
+            }
+            else
+            {
+                boat.transform.position = new Vector2(9, Random.Range(-2, -5));
+                boat.GetComponent<Rigidbody2D>().velocity = new Vector2(-2.5f, 0);
+            }
+            
+        }
     }
 
 }
